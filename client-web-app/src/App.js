3a0b8a982aa16eb85a8830c814bb09828c66ae2a
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-07 23:33:38 +0600

    CSS Reset Applied

--- client-web-app/src/App.js
+++ client-web-app/src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Cybr Click</h1>
+      <ul>
+        <li>bread</li>
+        <li>eggs</li>
+        <li>milk</li>
+      </ul>
     </div>
   );
 }
